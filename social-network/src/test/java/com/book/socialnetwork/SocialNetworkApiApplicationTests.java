07c55ee22326b78a9729d6ffbe5aaf646552d31b
yohagos <yosef.hagos@googlemail.com>
2024-04-20 13:28:42 +0200

    feat: init backend project

---
+++ social-network/src/test/java/com/book/socialnetwork/SocialNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
