4bcffe87c75e120542db5df535d42c948c493c42
yohagos <yosef.hagos@googlemail.com>
2024-04-21 17:18:37 +0200

    fix: update main class to enable entity listeners

--- social-network/src/main/java/com/book/socialnetwork/SocialNetworkApiApplication.java
+++ social-network/src/main/java/com/book/socialnetwork/SocialNetworkApiApplication.java
@@ -2,8 +2,10 @@ package com.book.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SocialNetworkApiApplication {
 
 	public static void main(String[] args) {
