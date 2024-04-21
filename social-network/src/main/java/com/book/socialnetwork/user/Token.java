94bdabff73231e5f9bf7cbd0c79e7f3725a98667
yohagos <yosef.hagos@googlemail.com>
2024-04-21 17:24:34 +0200

    feat: adding token model and repository with one method findByToken()

---
+++ social-network/src/main/java/com/book/socialnetwork/user/Token.java
@@ -0,0 +1,27 @@
+package com.book.socialnetwork.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
