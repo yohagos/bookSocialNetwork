75eb626979653a5b6b9eb548b8bfc87a785b9e06
yohagos <yosef.hagos@googlemail.com>
2024-04-21 18:50:44 +0200

    feat: added UserDetailsServiceImpl implementation

---
+++ social-network/src/main/java/com/book/socialnetwork/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.book.socialnetwork.security;
+
+import com.book.socialnetwork.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl  implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
