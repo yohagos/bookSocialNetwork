c335f73780b551e3b06f1ac8f3c464b63c1afd54
yohagos <yosef.hagos@googlemail.com>
2024-04-23 14:35:27 +0200

    feat: added book component

---
+++ social-network/src/main/java/com/book/socialnetwork/book/Book.java
@@ -0,0 +1,44 @@
+package com.book.socialnetwork.book;
+
+import com.book.socialnetwork.common.BaseEntity;
+import com.book.socialnetwork.feedback.Feedback;
+import com.book.socialnetwork.history.BookTransactionHistory;
+import com.book.socialnetwork.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+}
