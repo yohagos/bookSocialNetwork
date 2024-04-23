ae26ce0dbfd5927efafd0ced4036a706e5f7a0cc
yohagos <yosef.hagos@googlemail.com>
2024-04-23 14:36:05 +0200

    feat: added bookTransactionHistory component

---
+++ social-network/src/main/java/com/book/socialnetwork/history/BookTransactionHistory.java
@@ -0,0 +1,32 @@
+package com.book.socialnetwork.history;
+
+import com.book.socialnetwork.book.Book;
+import com.book.socialnetwork.common.BaseEntity;
+import com.book.socialnetwork.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    private boolean returned;
+    private boolean returnApproved;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
