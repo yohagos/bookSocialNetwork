52b9792c21494f249f78961ad2715ec3bbe8100f
yohagos <yosef.hagos@googlemail.com>
2024-04-23 14:35:43 +0200

    feat: added feedback component

---
+++ social-network/src/main/java/com/book/socialnetwork/feedback/Feedback.java
@@ -0,0 +1,28 @@
+package com.book.socialnetwork.feedback;
+
+import com.book.socialnetwork.book.Book;
+import com.book.socialnetwork.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double note;
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
