615d782adb54a0a840308172e1f397da275f6125
yohagos <yosef.hagos@googlemail.com>
2024-04-23 15:08:05 +0200

    fix: adjusting auditing through JpaAuditing

--- social-network/src/main/java/com/book/socialnetwork/SocialNetworkApiApplication.java
+++ social-network/src/main/java/com/book/socialnetwork/SocialNetworkApiApplication.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
 public class SocialNetworkApiApplication {
 
