5795fe04aabee4bd6ad05a314dccaaa97df8f6c0
yohagos <yosef.hagos@googlemail.com>
2024-04-23 15:07:22 +0200

    fix: adjusting beansConfig

--- social-network/src/main/java/com/book/socialnetwork/config/BeansConfig.java
+++ social-network/src/main/java/com/book/socialnetwork/config/BeansConfig.java
@@ -3,6 +3,7 @@ package com.book.socialnetwork.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -28,6 +29,11 @@ public class BeansConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<Long> auditorAware() {
+        return new ApplicationAuditAware();
     }
 
     @Bean
