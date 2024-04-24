9fc9340cb83aa6b2d89e1011c63b1b9b0d0c5905
yohagos <yosef.hagos@googlemail.com>
2024-04-24 17:53:40 +0200

    feat: added book transaction history repository

---
+++ social-network/src/main/java/com/book/socialnetwork/history/BookTransactionHistoryRepository.java
@@ -0,0 +1,17 @@
+package com.book.socialnetwork.history;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Long> {
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.user.id = :userId
+            """)
+    Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Long userId);
+}
