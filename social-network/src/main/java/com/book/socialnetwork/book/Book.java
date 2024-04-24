ae3e87eb03e13ccfc89c24ea91a54106e1113de8
yohagos <yosef.hagos@googlemail.com>
2024-04-24 17:49:36 +0200

    fix: adjusting book model and repository

--- social-network/src/main/java/com/book/socialnetwork/book/Book.java
+++ social-network/src/main/java/com/book/socialnetwork/book/Book.java
@@ -4,10 +4,7 @@ import com.book.socialnetwork.common.BaseEntity;
 import com.book.socialnetwork.feedback.Feedback;
 import com.book.socialnetwork.history.BookTransactionHistory;
 import com.book.socialnetwork.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,4 +38,15 @@ public class Book extends BaseEntity {
 
     @OneToMany(mappedBy = "book")
     private List<BookTransactionHistory> histories;
+
+    @Transient
+    public double getRate() {
+        if (feedbacks == null || feedbacks.isEmpty())
+            return 0.0;
+        var rate = feedbacks.stream()
+                .mapToDouble(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+        return Math.round(rate * 10.0) / 10;
+    }
 }
