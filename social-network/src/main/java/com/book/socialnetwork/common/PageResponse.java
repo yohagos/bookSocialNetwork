12bf3822797a99a85c1435cea45d236269958fd0
yohagos <yosef.hagos@googlemail.com>
2024-04-24 17:52:57 +0200

    feat: added page response for pageable responses

---
+++ social-network/src/main/java/com/book/socialnetwork/common/PageResponse.java
@@ -0,0 +1,21 @@
+package com.book.socialnetwork.common;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+
+    private List<T> content;
+    private int pageNumber;
+    private int sizeOfEachPage;
+    private long totalElement;
+    private int totalPage;
+    private boolean firstPage;
+    private boolean lastPage;
+}
