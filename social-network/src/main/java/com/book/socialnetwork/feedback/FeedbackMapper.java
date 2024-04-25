4ff9d0fa94f63505152244751c20fae72de5e87a
yohagos <yosef.hagos@googlemail.com>
2024-04-25 14:30:50 +0200

    feat: added feedback mapper

---
+++ social-network/src/main/java/com/book/socialnetwork/feedback/FeedbackMapper.java
@@ -0,0 +1,34 @@
+package com.book.socialnetwork.feedback;
+
+import com.book.socialnetwork.book.Book;
+import com.book.socialnetwork.feedback.requests.FeedbackRequest;
+import com.book.socialnetwork.feedback.responses.FeedbackResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.getComment())
+                .book(
+                        Book.builder()
+                                .id(request.bookId())
+                                .archived(false)
+                                .shareable(false)
+                                .build()
+                )
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback f, Long userId) {
+        return FeedbackResponse.builder()
+                .note(f.getNote())
+                .comment(f.getComment())
+                .ownFeedback(Objects.equals(f.getCreatedBy(), userId))
+                .build();
+    }
+}
