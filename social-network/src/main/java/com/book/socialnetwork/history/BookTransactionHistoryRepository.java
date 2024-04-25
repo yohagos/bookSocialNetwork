f5e2a0537b647a1fe8c780d06cd9a3f429efa8a3
yohagos <yosef.hagos@googlemail.com>
2024-04-25 13:12:51 +0200

    fix: added custom queries and methods for handling transactions

--- social-network/src/main/java/com/book/socialnetwork/history/BookTransactionHistoryRepository.java
+++ social-network/src/main/java/com/book/socialnetwork/history/BookTransactionHistoryRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Long> {
     @Query("""
@@ -14,4 +16,40 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
             WHERE history.user.id = :userId
             """)
     Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Long userId);
+
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.book.owner.id = :userId
+            """)
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Long userId);
+
+    @Query("""
+            SELECT (COUNT(*) > 0) AS isBorrowed
+            FROM BookTransactionHistory history
+            WHERE history.user.id = :userId
+            AND history.book.id = :bookId
+            AND history.returnApproved = false
+            """)
+    boolean isAlreadyBorrowedByUser(Long bookId, Long userId);
+
+    @Query("""
+            SELECT transaction
+            FROM BookTransactionHistory transaction
+            WHERE transaction.user.id = :userId
+            AND transaction.book.id = :bookId
+            AND transaction.returned = false
+            AND transaction.returnApproved = false
+            """)
+    Optional<BookTransactionHistory> findByBookIdAndUserId(Long bookId, Long userId);
+
+    @Query("""
+            SELECT transaction
+            FROM BookTransactionHistory transaction
+            WHERE transaction.book.owner.id = :ownerId
+            AND transaction.book.id = :bookId
+            AND transaction.returned = true
+            AND transaction.returnApproved = false
+            """)
+    Optional<BookTransactionHistory> findByBookIdAndOwnerId(Long bookId, Long ownerId);
 }
