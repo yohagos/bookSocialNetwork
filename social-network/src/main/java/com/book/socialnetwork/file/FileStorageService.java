56f486538649fc8ff5af2e4668dffed048259e9b
yohagos <yosef.hagos@googlemail.com>
2024-04-25 13:12:14 +0200

    feat: added FileStorageService for handling and saving book cover images

---
+++ social-network/src/main/java/com/book/socialnetwork/file/FileStorageService.java
@@ -0,0 +1,69 @@
+package com.book.socialnetwork.file;
+
+import com.book.socialnetwork.book.Book;
+import jakarta.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.io.File.separator;
+import static java.lang.System.currentTimeMillis;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FileStorageService {
+
+    @Value("${application.file.upload.photos-upload-path}")
+    private String fileUploadPath;
+
+    public String saveFile(
+            @Nonnull MultipartFile sourceFile,
+            @Nonnull Long userId) {
+        final  String fileUploadSubPath = "users" + separator + userId;
+        return uploadFile(sourceFile, fileUploadSubPath);
+    }
+
+    private String uploadFile(
+            @Nonnull MultipartFile sourceFile,
+            @Nonnull String fileUploadSubPath
+    ) {
+        final String finalUploadPath = this.fileUploadPath + separator + fileUploadSubPath;
+        File targetFolder = new File(finalUploadPath);
+        if (!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if (!folderCreated) {
+                log.warn("Failed to create target folder");
+                return null;
+            }
+        }
+        final String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
+        String targetFilePath = finalUploadPath + separator + currentTimeMillis() + "." + fileExtension;
+        Path targetPath = Paths.get(targetFilePath);
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("File saved to " + targetFilePath);
+            return targetFilePath;
+        } catch (IOException e) {
+            log.warn("File was not saved " + e.getMessage());
+        }
+        return null;
+    }
+
+    private String getFileExtension(String filename) {
+        if (filename == null || filename.isEmpty())
+            return "";
+        int lastDotIndex = filename.lastIndexOf(".");
+        if (lastDotIndex == -1)
+            return "";
+        return filename.substring(lastDotIndex + 1).toLowerCase();
+    }
+}
