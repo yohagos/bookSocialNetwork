25b01e882ac9e226ab7403d4d57377843b949369
yohagos <yosef.hagos@googlemail.com>
2024-04-26 16:06:22 +0200

    feat: added corsConfiguration Bean to fix cors issues

--- social-network/src/main/java/com/book/socialnetwork/config/BeansConfig.java
+++ social-network/src/main/java/com/book/socialnetwork/config/BeansConfig.java
@@ -11,6 +11,15 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -39,5 +48,34 @@ public class BeansConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(
+                Collections.singletonList("http://localhost:4200")
+        );
+        config.setAllowedHeaders(
+                Arrays.asList(
+                        ORIGIN,
+                        CONTENT_TYPE,
+                        ACCEPT,
+                        AUTHORIZATION
+                )
+        );
+        config.setAllowedMethods(
+                List.of(
+                        "GET",
+                        "POST",
+                        "PATCH",
+                        "DELETE",
+                        "PUT"
+                )
+        );
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
