cf2458dca33e9b3bca18ed97ee7f5c579467d370
yohagos <yosef.hagos@googlemail.com>
2024-04-26 15:13:52 +0200

    fix: adjusting frontend settings and added a route to app-routing.module.ts

--- book-social-network-ui/src/app/app.module.ts
+++ book-social-network-ui/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialsModule } from "./materials.module";
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { LoginComponent } from "./core/login/login.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     MaterialsModule,
     HttpClientModule
