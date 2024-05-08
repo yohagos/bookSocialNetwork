325fdf6cea91487935d54b8f38b662990a4e01ef
yohagos <yosef.hagos@googlemail.com>
2024-05-08 19:28:50 +0200

    fix: update SecurityConfig for keycloak integration

--- social-network/src/main/java/com/book/socialnetwork/security/SecurityConfig.java
+++ social-network/src/main/java/com/book/socialnetwork/security/SecurityConfig.java
@@ -3,17 +3,13 @@ package com.book.socialnetwork.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +17,8 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
-    private final JwtFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
+    // private final JwtFilter jwtAuthFilter;
+    // private final AuthenticationProvider authenticationProvider;
 
     private final String[] WHITE_LIST = {
             "/auth/**",
@@ -53,11 +49,13 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                 )
-                .sessionManagement(
-                        session -> session.sessionCreationPolicy(STATELESS)
-                )
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .oauth2ResourceServer(
+                        auth -> auth.jwt(
+                                token -> token.jwtAuthenticationConverter(
+                                        new KeycloakJwtAuthenticationConverter()
+                                )
+                        )
+                );
         return http.build();
     }
 }
